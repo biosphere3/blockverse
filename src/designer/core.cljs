cf1bb381a43af0befa893edf0422d51f58e5acec
Michael Dougherty <maackle.d@gmail.com>
2015-08-23 18:58:00 -0700

    not-so-well-working editable input

--- src/designer/core.cljs
+++ src/designer/core.cljs
@@ -7,17 +7,22 @@
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(defn log [x] (.log js/console x))
 
 (defrecord Port [type name scalar units])
+
+#_(defn mkport [type name scalar units]
+  {:type type
+   :name name
+   :booboo {:boo :boo}
+   :scalar scalar
+   :units units})
 
 (def initial-state
   {:text "Hello world!!"
    :blocks [{:name "humanoid"
              :ports [(->Port :input "food" 360 "pounds per year")
                      (->Port :input "water" 2 "L per day")]}]})
-
-(println "INITIAL STATE:" initial-state)
 
 (def app-state (atom initial-state))
 
@@ -27,35 +32,51 @@
       om/IRender
       (render [this] (html (markup-fn cursor owner))))))
 
+(defcomponent editable-field [data owner {:keys [field]}]
+  (init-state [this] {:editing false})
+  (render [this]
+    (let [handle-edit (fn [e]
+                        (om/update! data field (.. e -target -value)))]
+      (log owner)
+      (log (:editing owner))
+      (if true
+        (html
+          [:input {:value (field data)
+                   :onChange handle-edit}])
+        (html
+          [:span {:onDoubleClick #(om/set-state! owner :editing true)} (field data)])))))
 
 (defcomponent port-view [port owner]
   (render [this]
           (html
-            [:.port
-             [:span "name "]
+            [:li.port
              [:select [:option "input"] [:option "output"]]
-             [:input {:value (:name port)}]
-             [:input {:value (:scalar port)}]
-             [:input {:value (:units port)}]
+             (om/build editable-field port {:opts {:field :name}})
+             (om/build editable-field port {:opts {:field :scalar}})
+             (om/build editable-field port {:opts {:field :units}})
+             #_[:input {:value (:name port)
+                      :onChange (handle-edit :name)
+                      :placeholder "name"}]
+             #_[:input {:value (:scalar port)}]
+             #_[:input {:value (:units port)}]
              ])))
 
 (defcomponent block-view [block owner]
   (render [this]
           (html
-            [:li
+            [:li.block
              [:label "name " [:input {:value (:name block)}]]
              [:ul.ports
-              [:h2 "ports "]
               (om/build-all port-view (:ports block))
-              #_(for [port (:ports block)]
-                  (html-port port))]])))
+              ]])))
 
 (om/root
   (fn [data owner]
     (reify om/IRender
       (render [_]
               (html [:.form [:h1 "DESIGNER"]
-                     [:ul "blocks"
+                     [:ul
+                      [:h2 "blocks"]
                       (om/build-all block-view (:blocks data))]])
               )))
   app-state
