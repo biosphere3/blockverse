74c7f9f1f833675f46dc30c8efc2c2582cc62e3d
Michael Dougherty <maackle.d@gmail.com>
2015-08-23 21:06:05 -0700

    add/remove block

--- src/designer/core.cljs
+++ src/designer/core.cljs
@@ -1,7 +1,9 @@
 (ns ^:figwheel-always designer.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [cljs.pprint :refer (pprint)]
             [cljs.reader :refer (read-string)]
+            [cljs.core.async :refer [put! chan <!]]
             [om-tools.core :refer-macros [defcomponent]]
             [sablono.core :as html :refer-macros [html]]
             [om.dom :as dom :include-macros true]
@@ -38,8 +40,6 @@
   (render [this]
     (let [handle-edit (fn [e]
                         (om/update! data field (.. e -target -value)))]
-      (log owner)
-      (log (:editing owner))
       (if true
         (html
           [:input {:value (field data)
@@ -51,40 +51,59 @@
   (fn [e] (om/update! data field (f (.. e -target -value)))))
 
 (defcomponent port-view [port owner]
-  (render [this]
+  (render-state [this {:keys [comm]}]
           (html
             [:li.port
+             [:button {:onClick #(put! comm [:delete-port port])}]
              [:select [:option "input"] [:option "output"]]
              [:input {:value (:name port)
                       :onChange (edit-fn port :name str)
                       :placeholder "name"}]
              [:input {:value (:scalar port)
-                      :onChange (edit-fn port :scalar read-string)}]
+                      :onChange (edit-fn port :scalar read-string)}]  ; TODO use something better than read-string
              [:input {:value (:units port)
                       :onChange (edit-fn port :units str)}]
              ])))
 
 (defcomponent block-view [block owner]
-  (render [this]
+  (render-state [this {:keys [comm]}]
           (html
             [:li.block
+             [:button {:onClick #(put! comm [:delete-block @block])} "×"]
              [:label "name " [:input {:value (:name block)
                                       :onChange (edit-fn block :name str)}]]
-             [:ul.ports
-              (om/build-all port-view (:ports block))
+             [:ul.port-list
+              (om/build-all port-view (:ports block) {:init-state {:comm comm}})
+              [:button {:onClick (fn [] (om/transact! block :ports #(conj % {})))} "add one"]
               ]])))
 
+(defn handle-event
+  [app type value]
+  (case type
+    :delete-block (om/transact! app :blocks #(vec (remove (partial = value) %)))
+    :delete-port (log "TODO")))
+
+(defcomponent app [data owner]
+  (init-state [_] {:comm (chan)})
+  (will-mount
+    [_]
+    (let [comm (om/get-state owner :comm)]
+      (go (loop []
+            (let [[type value] (<! comm)]
+              (handle-event data type value)
+              (recur))))))
+  (render-state [_ state]
+    (html [:div
+           [:.container
+            [:h1 "DESIGNER"]
+            [:ul.block-list
+             [:h2 "blocks"]
+             (om/build-all block-view (:blocks data) {:init-state state})
+             [:button {:onClick (fn [] (om/transact! data :blocks #(conj % {})))} "add one"]]]
+           [:pre.debug (with-out-str (pprint @data))]])))
+
 (om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-              (html [:div
-                     [:.form [:h1 "DESIGNER"]
-                      [:ul
-                       [:h2 "blocks"]
-                       (om/build-all block-view (:blocks data))]]
-                     [:pre.debug (with-out-str (pprint @data))]])
-              )))
+  app
   app-state
   {:target (. js/document (getElementById "app"))})
 
